9c7998a91513eebaa06699ad16a80ade8f76514f
Michael Spencer <zequinox117@gmail.com>
2025-08-30 10:56:43 -0500

    commit 2

--- components/html_and_css_handler.js
+++ components/html_and_css_handler.js
@@ -1,90 +1,74 @@
 export default class HtmlAndCssHandler {
-    constructor() {
+    constructor(dimensions, pixelSize) {
+        this.dimensions = dimensions
+        this.pixelSize = pixelSize
+        this.minX = 11
+        this.maxX = 112
+        this.minY = 14
+        this.maxY = 152
+        this.minPixel = 2
+        this.maxPixel = 8
     }
     setTitle() {
         document.title = "Slope Intercept Formula"
     }
-    // attachCSS() {
-    //     const style = document.createElement("style")
-    //     style.innerHTML = `
-    //         body {
-    //             background: #0f0f0f;
-    //         }
-    //         #game-container {
-    //             display: flex;
-    //             flex-direction: column;
-    //             align-items: center;
-    //             margin-top: 120px;
-    //             /* position: relative; */
-    //         }
-    //         #start-button {
-    //             /* position: absolute;
-    //             top: 50%;
-    //             left: 50%; */
-    //         }
-    //         #moniter {
-    //             display: flex;
-    //             border: white 4px solid;
-    //         }
-    //         #score {
-    //             color: #fff;
-    //             font-size: 25px;
-    //         }
-    //     `
-    //     document.head.appendChild(style)
-    // }
-    // attachHTML() {
-    //     const root = document.createElement('div')
-    //     const gamesContainerDiv = document.createElement('div')
-    //     gamesContainerDiv.setAttribute('id', "game-container")
-
-    //     const startButton = document.createElement('button')
-    //     startButton.setAttribute('id', 'startButton')
-    //     startButton.innerHTML = this.startButtonsText
-    //     gamesContainerDiv.appendChild(startButton)
-    //     const score = document.createElement('div')
-    //     score.setAttribute('id', 'score')
-    //     gamesContainerDiv.appendChild(score)
-    //     const moniter = document.createElement('div')
-    //     moniter.setAttribute('id', 'moniter')
-    //     gamesContainerDiv.appendChild(moniter)
-
-    //     root.setAttribute('id', 'root')
-    //     root.appendChild(gamesContainerDiv)
-    //     document.body.appendChild(root)
-
-    //     if(this.gameSize < 7 || this.gameSize > 31 || this.gameSize % 2 == 0) {
-    //         console.error('invalid gameboard size, gameboard size must an odd numbered integer between 7 and 31')
-    //         return
-    //     }
-
-    //     let matrix = []
-    //     for(let i = 0; i < this.gameSize; i++) {
-    //     let pixelRowContainer = document.createElement('div')
-    //     matrix.push([])
-    //         for(let j = 0; j < this.gameSize; j++) {
-    //             let pixel = document.createElement('div')
-    //             pixel.style.width = this.pixelSize + "px"
-    //             pixel.style.height = this.pixelSize + "px"
-    //             pixel.style.border = "white 1px solid"
-    //             pixelRowContainer.appendChild(pixel)
-
-    //             matrix[i].push({
-    //                 color: null
-    //             })
-    //         }
-    //         moniter.appendChild(pixelRowContainer)
-    //     }
-    //     return matrix
-    // }
-    // attachStartButtonsEventHandler() {
-    //     startButton.addEventListener('click', () => this.startButtonsCallback())
-    // }
+    attachCSS() {
+        const style = document.createElement("style")
+        style.innerHTML = `
+            body {
+                background: #0f0f0f;
+            }
+            #game-container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                margin-top: 120px;
+                /* position: relative; */
+            }
+            #start-button {
+                /* position: absolute;
+                top: 50%;
+                left: 50%; */
+            }
+            #moniter {
+                display: flex;
+                border: white 4px solid;
+            }
+            #score {
+                color: #fff;
+                font-size: 25px;
+            }
+        `
+        document.head.appendChild(style)
+    }
+    invalidDimensions() {
+        if(this.dimensions[0] < this.minX || this.dimensions[0] > this.maxX) {
+            return true
+        }
+        if(this.dimensions[1] < this.minY || this.dimensions[1] > this.maxY) {
+            return true
+        }
+    }
+    invalidPixelSize() {
+        if(this.pixelSize < this.minPixel || this.pixelSize > this.maxPixel) {
+            return true
+        }
+    }
+    attachDisplay() {
+        if(this.invalidDimensions()) {
+            console.error(`Invalid dimensions, dimension 1 must be between ${ this.minX } and ${ this.maxX } and dimension 2 must be between ${ this.minY } and ${ this.maxY }`)
+            return
+        }
+        if(this.invalidPixelSize()) {
+            console.error(`Invalid pixel size, pixel size must be between ${ this.minPixel } and ${ this.maxPixel }`)
+            return
+        }
+    }
     main() {
         this.setTitle()
-        // this.attachCSS()
-        // let matrix = this.attachHTML()
-        // this.attachStartButtonsEventHandler()
-        // return matrix
+        this.attachCSS()
+        this.attachDisplay()
+        let matrix = this.attachDisplay()
+        return matrix
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import HtmlAndCssHandler from "./components/html_and_css_handler.js"
 
 export default class App {
+    constructor(config) {
+        this.config = config
+        this.matrix
+    }
     start() {
-        new HtmlAndCssHandler().main()
+        this.matrix = new HtmlAndCssHandler(this.config.dimensions, this.config.pixelSize).main()
     }
 }
