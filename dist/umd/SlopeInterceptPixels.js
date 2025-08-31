2bf10701d17445383fb5cefb2d8fe28ef1ea3087
Michael Spencer <zequinox117@gmail.com>
2025-08-30 21:36:46 -0500

    commit

--- dist/umd/SlopeInterceptPixels.js
+++ dist/umd/SlopeInterceptPixels.js
@@ -17,16 +17,37 @@
         }
         Controller.prototype.setMandB = function () {
             this.M = ((this.defaultPoints[0][1] - 0.5) - (this.defaultPoints[1][1] - 0.5)) / ((this.defaultPoints[0][0] - 0.5) - (this.defaultPoints[1][0] - 0.5));
-            this.B = (this.defaultPoints[0][1] - 0.5) + (this.M * (this.defaultPoints[0][0] - 0.5));
+            // y = mx + b
+            this.B = this.defaultPoints[0][1] - (this.M * this.defaultPoints[0][0]);
         };
         Controller.prototype.drawStartingPoints = function () {
-            this.$display.children[this.defaultPoints[0][1] - 1].children[this.defaultPoints[0][0] - 1].classList.add('starting-point');
-            this.$display.children[this.defaultPoints[1][1] - 1].children[this.defaultPoints[1][0] - 1].classList.add('starting-point');
+            this.$display.children[this.defaultPoints[0][0]].children[this.defaultPoints[0][1]].classList.add('starting-point');
+            this.$display.children[this.defaultPoints[1][0]].children[this.defaultPoints[1][1]].classList.add('starting-point');
+        };
+        Controller.prototype.findPixels = function () {
+            for (var i = 0; i < this.dimensions[0]; i++) {
+                for (var j = 0; j < this.dimensions[1]; j++) {
+                    var x = i;
+                    var y = j;
+                    this.checkIfPixelContainsPartOfLine(x, y);
+                }
+            }
+        };
+        Controller.prototype.checkIfPixelContainsPartOfLine = function (x, y) {
+            var val1 = (this.M * x) + this.B;
+            var val2 = (this.M * (x + 1) + this.B);
+            if ((val1 <= y) && (val2 >= (y + 1))) {
+                console.log('point on line', x, y, this.M, this.B);
+                var areaOfGraph = this.$display.children[x].children[y];
+                if (!areaOfGraph.classList.contains('starting-point')) {
+                    areaOfGraph.classList.add('part-of-the-line');
+                }
+            }
         };
         Controller.prototype.main = function () {
             this.drawStartingPoints();
             this.setMandB();
-            console.log('hello world!!!', this.dimensions);
+            this.findPixels();
         };
         return Controller;
     }());
--- src/components/controller.ts
+++ src/components/controller.ts
@@ -16,15 +16,36 @@ export default class Controller {
     }
     setMandB() {
         this.M = ((this.defaultPoints[0][1] - 0.5) - (this.defaultPoints[1][1] - 0.5)) / ((this.defaultPoints[0][0] - 0.5) - (this.defaultPoints[1][0] - 0.5))
-        this.B = (this.defaultPoints[0][1] - 0.5) + (this.M * (this.defaultPoints[0][0] - 0.5))
+        // y = mx + b
+        this.B = this.defaultPoints[0][1] - (this.M * this.defaultPoints[0][0])
     }
     drawStartingPoints() {
-        this.$display.children[this.defaultPoints[0][1]-1].children[this.defaultPoints[0][0]-1].classList.add('starting-point')
-        this.$display.children[this.defaultPoints[1][1]-1].children[this.defaultPoints[1][0]-1].classList.add('starting-point')
+        this.$display.children[this.defaultPoints[0][0]].children[this.defaultPoints[0][1]].classList.add('starting-point')
+        this.$display.children[this.defaultPoints[1][0]].children[this.defaultPoints[1][1]].classList.add('starting-point')
+    }
+    findPixels() {
+        for(let i = 0; i < this.dimensions[0]; i++) {
+            for(let j = 0; j < this.dimensions[1]; j++) {
+                let x = i
+                let y = j
+                this.checkIfPixelContainsPartOfLine(x, y)
+            }
+        }
+    }
+    checkIfPixelContainsPartOfLine(x: number, y: number) {
+        let val1 = (this.M * x) + this.B
+        let val2 = (this.M * (x + 1) + this.B)
+        if((val1 <= y) && (val2 >= (y + 1))) {
+            console.log('point on line', x, y, this.M, this.B)
+            let areaOfGraph = this.$display.children[x].children[y]
+            if(!areaOfGraph.classList.contains('starting-point')) {
+                areaOfGraph.classList.add('part-of-the-line')
+            }
+        }
     }
     main() {
         this.drawStartingPoints()
         this.setMandB()
-        console.log('hello world!!!', this.dimensions)
+        this.findPixels()
     }
 }
